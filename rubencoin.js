50dd0976807f8b90d52841584cb83cd0ac5137ad
cssmith07060 <cssmith07060@gmail.com>
2021-12-12 20:36:06 -0500

    new changes

--- rubencoin.js
+++ rubencoin.js
@@ -1,9 +1,16 @@
 const Sha256 = require('crypto-js/sha256');
 
+class Transaction{
+    constructor(fromAddress, toAddress, amount) {
+        this.fromAddress = fromAddress;
+        this.toaddress = this.toaddress;
+        this.amount = amount;
+    }
+}
+
 
 class Block{
     constructor(index, timestamp, data, previoushash = '') {
-        this.index = index;
         this.timestamp = timestamp;
         this.data = data;
         this.previousHash = this.previousHash;
@@ -15,7 +22,8 @@ class Block{
     }
     mineBlock(difficulty) {
         while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
-            
+            this.non++;
+            this.hash = this.calculateHash();
         }
     }
 }
@@ -24,9 +32,12 @@ class Block{
 class Blockchain{
     constructor() {
         this.chain = [this.createGensasisBlock()];
+        this.difficulty = 5;
+        this.pendingTransactions = [];
+        this.miningReward = 100;
     }
     createGenesisBlock() {
-        return new Block(0, "01/01/2017", "Genasis block", "0");
+        return new Block("01/01/2017", "Genasis block", "0");
     }
     getLatestBlock() {
         return this.chain[this.chain.length - 1];
@@ -35,7 +46,7 @@ class Blockchain{
         newBlock.previous = this.getlastestBlock().hash;
         this.chain.push(newBlock);
     }
-    isthisChainValid() {
+    isChainValid() {
         for (let i = 1; 1 < this.chain.length; 1++){
             const currentBlock = this.chain[i];
             const previousBlock = this.chain[i - 1];
@@ -57,5 +68,7 @@ class Blockchain{
 let savjeeCoin = new Blockchain();
 savjeeCoin.addBlock(new Block(1, "10/07/2017", { amount: 4 })); 
 savjeeCoin.addBlock(new Block(2, "12/07/2017", { amount: 10 }));
+savjeeCoin.chain[1].data = { amount: 100 };
+savjeeCoin.chain[1].hash = savjeeCoin.chain.[1].calculateHash();
 //consol.log('Is blockchain valid?')
 //console.log(JSON.stringify(rubenCOin, null, 4));
