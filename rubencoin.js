7011436892336250a8844b7d1b3e166aad487534
cssmith07060 <cssmith07060@gmail.com>
2022-05-16 19:12:51 -0400

    next

--- rubencoin.js
+++ rubencoin.js
@@ -31,19 +31,20 @@ class Block{
 
 class Blockchain{
     constructor() {
-        this.chain = [this.createGensasisBlock()];
+        this.chain = [this.createGensesisBlock()];
         this.difficulty = 5;
         this.pendingTransactions = [];
         this.miningReward = 100;
     }
     createGenesisBlock() {
-        return new Block("01/01/2017", "Genasis block", "0");
+        return new Block("01/01/2017", "Genesis block", "0");
     }
     getLatestBlock() {
         return this.chain[this.chain.length - 1];
     }
     addBlock(newBlock) {
         newBlock.previous = this.getlastestBlock().hash;
+        newBlock.mineBlock(this.difficulty);
         this.chain.push(newBlock);
     }
     isChainValid() {
@@ -68,7 +69,3 @@ class Blockchain{
 let savjeeCoin = new Blockchain();
 savjeeCoin.addBlock(new Block(1, "10/07/2017", { amount: 4 })); 
 savjeeCoin.addBlock(new Block(2, "12/07/2017", { amount: 10 }));
-savjeeCoin.chain[1].data = { amount: 100 };
-savjeeCoin.chain[1].hash = savjeeCoin.chain.[1].calculateHash();
-//consol.log('Is blockchain valid?')
-//console.log(JSON.stringify(rubenCOin, null, 4));
